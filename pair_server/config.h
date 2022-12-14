7a776ddd281708bbc616b42228dc3cb35755d070
Andrey-Sha <shaxov.00002@mail.ru>
2022-12-14 15:43:04 +0500

    delete_item

---
+++ pair_server/config.h
@@ -0,0 +1,7 @@
+#ifndef MY_CONFIG_H
+#define MY_CONFIG_H
+
+#define INFO
+#define DEBUG
+
+#endif
--- pair_server/storage.c
+++ pair_server/storage.c
@@ -2,12 +2,16 @@
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
-#include "mbedtls/sha256.h"
-
 #include <stdio.h>
+#include "config.h"
+#include "mbedtls/sha256.h"
 
 #define SHA256 (0)
 #define TABLE_SIZE (4096)
+
+#if defined(DEBUG) || defined(INFO)
+static const char *TAG = "STORAGE";
+#endif
 
 static mbedtls_sha256_context ctx; 
 
@@ -25,7 +29,11 @@ static int calculate_sha256(const unsigned char *key, unsigned long long *hash)
 {
     int err = 0;
     unsigned char _hash[64];
-    
+
+#ifdef INFO
+    printf("%s: called calculate_sha256\n", TAG);
+#endif
+
     mbedtls_sha256_init(&ctx);
      
     err = mbedtls_sha256_starts(&ctx, SHA256);
@@ -54,23 +62,46 @@ int get_item(const char* key, char* out_value, size_t out_len)
     int err = 0;
     struct cell *current; 
     unsigned long long hash;
-    
+
+#ifdef INFO
+    printf("%s: called get_item\n", TAG);
+#endif
+
     out_value = NULL;
     
     err = calculate_sha256(key, &hash);
     if (err != 0) {
+#ifdef DEBUG
+        printf("%s: calculate_sha256 is failed in get_item\n", TAG);
+#endif
         errno = EINVAL;
         return err;
     }
 
-    //Добавить обработку для элемента в массиве
+    if (hash_table[hash].init_cell && strcmp(key, hash_table[hash].key) == 0) {
+        if (out_len >= strlen(hash_table[hash].value)) {
+            strcpy(out_value, hash_table[hash].value);
+            return 0;
+        } else {
+            errno = ENOMEM;
+#ifdef DEBUG
+            printf("%s: out of memory for out buffer in get_item\n", TAG);
+#endif
+            return -1;
+        }
+    } else {
+        current = hash_table[hash].next_cell;
+    }
 
-    for (current = current->next_cell; current != NULL; current = current->next_cell) {
+    for (;current != NULL; current = current->next_cell) {
         if (strcmp(key, current->key) == 0) {
             if (out_len >= strlen(current->value)) {
                 strcpy(out_value, current->value);
                 return 0;
             } else {
+#ifdef DEBUG
+                printf("%s: out of memory for out buffer in get_item\n", TAG);
+#endif
                 errno = ENOMEM;
                 return -1;
             }
@@ -85,9 +116,16 @@ int insert_item(const char* key, const char* value)
     int err = 0;
     struct cell *current, *prev; 
     unsigned long long hash;
-    
+
+#ifdef INFO
+    printf("%s: called insert_item\n", TAG);
+#endif
+
     err = calculate_sha256(key, &hash);
     if (err != 0) {
+#ifdef DEBUG
+        printf("%s: calculate_sha256 is failed in insert_item\n", TAG);
+#endif
         errno = EINVAL;
         return err;
     }
@@ -120,7 +158,45 @@ int insert_item(const char* key, const char* value)
 
 int delete_item(const char* key)
 {
-    return 0;
+    int err = 0;
+    struct cell *current, *prev, *next; 
+    unsigned long long hash;
+
+#ifdef INFO
+    printf("%s: called delete_item\n", TAG);
+#endif
+
+    err = calculate_sha256(key, &hash);
+    if (err != 0) {
+#ifdef DEBUG
+        printf("%s: calculate_sha256 is failed in delete_item\n", TAG);
+#endif
+        errno = EINVAL;
+        return err;
+    }
+
+    if (hash_table[hash].init_cell && strcmp(key, hash_table[hash].key) == 0) {
+        hash_table[hash].init_cell = 0; 
+        return 0;
+    } else {
+        current = hash_table[hash].next_cell;
+    }
+    
+    for (; current != NULL; current = current->next_cell) {
+        if (strcmp(key, current->key) == 0) {
+            prev = current->prev_cell;
+            next = current->next_cell;
+            free(current->value);
+            free(current);
+            prev->next_cell = next;
+            if (next) {
+                next->prev_cell = prev
+            }
+            return 0;
+        }
+    }
+
+    return -1;
 }
 
 int pop_item(const char* key, char* out_value)
